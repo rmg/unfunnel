8802923dc004c0311405ecbbf98e0046a130e08d
Ryan Graham <r.m.graham@gmail.com>
2013-04-17 13:49:00 -0700

    Reorganize tests

--- test/index.js
+++ test/index.js
@@ -12,7 +12,7 @@ function assertReceives(eStream, eString, done) {
 }
 
 describe("Unfunnel", function () {
-  it("should handle multiple streams", function (done) {
+  describe("over stream.PassThrough streams", function () {
     var north = new stream.PassThrough()
       , south = new stream.PassThrough()
       , a = new Unfunnel(north, south)
@@ -22,59 +22,64 @@ describe("Unfunnel", function () {
       , bar_a = a.endpoint('bar')
       , bar_b = b.endpoint('bar')
       , count = 0
-    function inc(err, something) {
+      , ender
+      , inc = function (err, something) {
       count += 1
       if (err)
         throw err
       if (count == 4)
-        done()
+        ender()
     }
-    foo_a.on('readable', assertReceives(foo_a, "foo_b", inc))
-    foo_b.on('readable', assertReceives(foo_b, "foo_a", inc))
-    bar_a.on('readable', assertReceives(bar_a, "bar_b", inc))
-    bar_b.on('readable', assertReceives(bar_b, "bar_a", inc))
-    foo_a.write("foo_a")
-    foo_b.write("foo_b")
-    bar_a.write("bar_a")
-    bar_b.write("bar_b")
+    it("should handle multiple streams", function (done) {
+      ender = done
+      foo_a.on('readable', assertReceives(foo_a, "foo_b", inc))
+      foo_b.on('readable', assertReceives(foo_b, "foo_a", inc))
+      bar_a.on('readable', assertReceives(bar_a, "bar_b", inc))
+      bar_b.on('readable', assertReceives(bar_b, "bar_a", inc))
+      foo_a.write("foo_a")
+      foo_b.write("foo_b")
+      bar_a.write("bar_a")
+      bar_b.write("bar_b")
+    })
   })
-
-  it("should stream multiple streams over a single duplex stream", function (done) {
-    var read_count = 0
-      , inc = function (err) {
-          read_count += 1
-          if (err)
-            throw err
-          if (read_count == 4) {
-            client.end()
+  describe("over a single duplex stream (net)", function () {
+    it("should stream multiple streams over a single duplex stream", function (done) {
+      var read_count = 0
+        , inc = function (err) {
+            read_count += 1
+            if (err)
+              throw err
+            if (read_count == 4) {
+              client.end()
+            }
           }
+        , close_count = 0
+        , close = function (err) {
+            close_count += 1
+            if (err)
+              throw err
+            if (close_count == 2) {
+              done()
+            }
         }
-      , close_count = 0
-      , close = function (err) {
-          close_count += 1
-          if (err)
-            throw err
-          if (close_count == 2) {
-            done()
+        , testConn = function(a, b, outer_conn) {
+            return function (conn) {
+              conn = conn || outer_conn
+              var mux = new Unfunnel(conn)
+                , foo = mux.endpoint('foo')
+                , bar = mux.endpoint('bar')
+              conn.on('end', close)
+              foo.on('readable', assertReceives(foo, "foo_" + b, inc))
+              bar.on('readable', assertReceives(bar, "bar_" + b, inc))
+              foo.write("foo_" + a)
+              bar.write("bar_" + a)
+            }
           }
-      }
-      , testConn = function(a, b, outer_conn) {
-          return function (conn) {
-            conn = conn || outer_conn
-            var mux = new Unfunnel(conn)
-              , foo = mux.endpoint('foo')
-              , bar = mux.endpoint('bar')
-            conn.on('end', close)
-            foo.on('readable', assertReceives(foo, "foo_" + b, inc))
-            bar.on('readable', assertReceives(bar, "bar_" + b, inc))
-            foo.write("foo_" + a)
-            bar.write("bar_" + a)
-          }
-        }
-      , net = require('net')
-      , server = net.createServer(testConn('north', 'south'))
-    server.listen(12345)
-    var client = net.createConnection(12345)
-    client.on('connect', testConn('south', 'north', client) )
+        , net = require('net')
+        , server = net.createServer(testConn('north', 'south'))
+      server.listen(12345)
+      var client = net.createConnection(12345)
+      client.on('connect', testConn('south', 'north', client) )
+    })
   })
 })
